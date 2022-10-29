ac9470f8cbae9428fd7cc781fdf1e2866a1655ee
HuynhPhuocTri <103299118+HuynhPhuocTri@users.noreply.github.com>
2022-10-29 18:20:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Mẹ gà';
+const giftUrl = 'https://www.facebook.com/tri.phuoc.3950';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Mẹ gà';
+const contentCard = '“Mẹ yêu ! Sinh nhật của mẹ con chúc mẹ mạnh khỏe và những may mắn, hạnh phúc nhất sẽ đến với mẹ trong cuộc đời này.s ”.';
+
+const giftImage = 'Mẹ gà.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
